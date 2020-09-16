0b2e0cc372aed13c9b71542b609a3e0bcc080abe
Hsin-Hung Wu <henrywu19970511@gmail.com>
2020-09-16 15:47:53 +0800

    pass all library tests

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,15 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    ret = []
+    with open(csv_file_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            ret.append([int(i) if i.isnumeric() else i for i in row])
+
+
+    return ret
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,28 @@
+import math
+
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    return math.sqrt(sum([(i[0] - i[1])**2 for i in zip(x,y)]))
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum([abs(i[0] - i[1]) for i in zip(x,y)])
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+
+    union = len(list(set.union(set(x), set(y))))
+    inter = len(list(set.intersection(set(x), set(y))))
+
+    print(inter)
+
+    if union == 0 or inter == 0:
+        return 1
+    return (union - inter)/(union)
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    numer = sum([i[0]*i[1]  for i in zip(x,y)])
+    deno = (sum(list(map(math.sqrt,x))) + sum(list(map(math.sqrt,y))))
+    if deno == 0 or x == y:
+        return 1
+    return numer/deno
 
 # Feel free to add more
+print(jaccard_dist([0,0], [1,0]))
